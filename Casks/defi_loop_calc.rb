98fe3193e5bbacb1cc850a172df0deb909531311
defijoker <defijoker@pm.me>
2025-02-07 04:19:01 +0400

    Update

--- Casks/defi_loop_calc.rb
+++ Casks/defi_loop_calc.rb
@@ -1,6 +1,6 @@
 cask "defi_loop_calc" do
     version "1.0"
-    sha256 "35ce91cedff62cd1b43fd0e64e5f79aafd5c62695b5ffd6e6263ca5dac3144eb"
+    sha256 "8f6aa65485c70c940aa099c6b98dfe8fbe6d44b746f64f69b249fa01dac7aa13"
   
     url "https://github.com/defijoker/homebrew-defi_loop_calc/releases/download/v1.0/final_prod_v1.dmg"
     name "defi_loop_calc"
