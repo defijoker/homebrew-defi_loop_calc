2349bcdd854860c730e2aa47f47c9a8687a12f13
defijoker <defijoker@pm.me>
2025-02-07 04:06:43 +0400

    Update download link

--- Casks/defi_loop_calc.rb
+++ Casks/defi_loop_calc.rb
@@ -2,7 +2,7 @@ cask "defi_loop_calc" do
     version "1.0"
     sha256 "35ce91cedff62cd1b43fd0e64e5f79aafd5c62695b5ffd6e6263ca5dac3144eb"
   
-    url "https://github.com/defijoker/homebrew-defi_loop_calc/blob/main/final_prod_v1.dmg"
+    url "https://github.com/defijoker/homebrew-defi_loop_calc/releases/download/v1.0/final_prod_v1.dmg"
     name "defi_loop_calc"
     desc "Defi Lending/Borrow Loop Calculator V1.0"
     homepage "https://github.com/defijoker/homebrew-defi_loop_calc/"
